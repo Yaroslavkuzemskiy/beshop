db56f5d184123d5322204f974bfb7dd713d18ae9
Your Name <you@example.com>
2022-11-01 16:35:36 +0200

    footer ready

--- script/script.js
+++ script/script.js
@@ -12,6 +12,7 @@ function closePromoBlock(){
 
 
 // open close mobile menu
+const mainHeader = document.querySelector('.header')
 const mobileBtn = document.querySelector('.phone-btn')
 let mobileBtnOpened = document.querySelector('.phone-btn')
 const listMenu = document.querySelector('.header-main-menu')
@@ -26,14 +27,50 @@ function openMenuMobile(){
         additionaltMenu.style.right = 106 + '%'
         headerMain.style.boxShadow  =  'none'
         headerMain.style.background  =  'none'
-        additionaltMenu.style.transition = 'all .2s ease-in-out';
+        additionaltMenu.style.transition = 'all .4s ease-in-out';
+        headerGrid.style.transition = 'all 1.1s ease-in-out';
+        mainHeader.style.zIndex = '0'
+        
 
     } else {
         mobileBtnOpened.classList.add('opened')
         headerGrid.style.left = 0
         additionaltMenu.style.right = 0
         additionaltMenu.style.transition = 'all 1.1s ease-in-out';
+        headerGrid.style.transition = 'all .5s ease-in-out';
         headerMain.style.boxShadow  =  '11px 200px 30px 11px rgb(74 74 76 / 90%)'
         headerMain.style.background  =  '#f5e3e0'
+        mainHeader.style.zIndex = '100'
     }
 }
+
+//menu open close footer menu
+
+const openCloseBtn = document.querySelectorAll('.footer-mobile-opener')
+const subMenu = document.querySelectorAll('.footer-submenu')
+const subMenuL = document.querySelector('.submenu')
+const subMenuItem = document.querySelector('.active')
+const liItem = document.querySelectorAll('.footer-mobile-opener svg')
+
+openCloseBtn.forEach((item, i) =>
+    item.addEventListener('click', () => {
+        
+        if (subMenu[i].classList.contains('active')){
+            subMenu[i].classList.remove('active')
+            subMenu[i].style.height = 0 + 'px'
+            liItem[i].style.transform = 'rotate(0deg)'
+        } else {
+            subMenu.forEach((it) => {
+                it.classList.remove('active')
+                it.style.height = 0 +'px'
+                liItem[i].style.transform = 'rotate(0deg)'
+            })
+            subMenu[i].classList.add('active')
+            let heightSubmenu =  subMenu[i].querySelectorAll('.submenu')
+            subMenu[i].style.height = heightSubmenu.length * subMenuL.offsetHeight + 'px'
+            liItem[i].style.transform = 'rotate(183deg)'
+        }
+       
+        
+    }
+    ))
